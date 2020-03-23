18708aeb4fd9a87fd9bbf3dcb15408d43a7bbd80
IepIweidieng <37586669+IepIweidieng@users.noreply.github.com>
2020-03-23 21:21:01 +0800

    fix(week_2): Fix suboptimal grammar and formatting
    - week_2/note.md
        - `marshal` does not require capitalization.
        - Use either `marshalling` or `marshaling` but not both.
        - Lowercase some words which does not require capitalization.
        - `imaginary litera` -> `imaginary part`
            - `imag()` does not construct a pure imaginary number;
                rather, it returns the imaginary part of a complex number.
        - Fix suboptimal grammar
        - Ensure a space between Halfwidth and Fullwidth words.
        - Fix a broken code block ended with four backquotes.
    
    - week_2/ch_3/operators/operators.go
        - Fix missing and redandunt spaces between words.
    
    - week_2/ch_3/string/string.go
        - Ensure a space between Halfwidth and Fullwidth words.
    
    - week_2/ch_4/Marshal/Marshal.go -> week_2/ch_4/marshal/marshal.go
        - `marshal` does not require capitalization.

--- week_2/ch_3/operators/operators.go
+++ week_2/ch_3/operators/operators.go
@@ -8,9 +8,9 @@ var y uint8 = 1<<1 | 1<<2
 func main() {
 	fmt.Printf("%08b\n", x)    // "00100010", the set {1, 5}
 	fmt.Printf("%08b\n", y)    // "00000110", the set {1, 2}
-	fmt.Printf("%08b\n", x&y)  // "00000010", theintersection {1}
+	fmt.Printf("%08b\n", x&y)  // "00000010", the intersection {1}
 	fmt.Printf("%08b\n", x|y)  // "00100110", the union {1, 2, 5}
-	fmt.Printf("%08b\n", x^y)  // "00100100", the symmetric  difference {2, 5}
+	fmt.Printf("%08b\n", x^y)  // "00100100", the symmetric difference {2, 5}
 	fmt.Printf("%08b\n", x&^y) // "00100000", the difference {5}
 	for i := uint(0); i < 8; i++ {
 		if x&(1<<i) != 0 { // membership test
--- week_2/ch_3/string/string.go
+++ week_2/ch_3/string/string.go
@@ -6,7 +6,7 @@ func main() {
 	s := "CCNSccns"
 	fmt.Println(len(s))     // "8"
 	fmt.Println(s[0], s[4]) // "67 99" ('C' and 'c')
-	//ASCII有32個控制字元同字母大小寫因此差32
+        //ASCII 有 32 個控制字元，同字母大小寫因此差 32
 	// c := s[len(s)]          // panic: index out of range
 	// fmt.Println(c)
 
